1f13f06085b7feea12e55121d62d54656def63f1
Md. Minar Munshi <minarmunshi46@gmail.com>
2023-08-26 00:47:26 +0600

    Create Onboarding Screen

--- App.js
+++ App.js
@@ -1,9 +1,38 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Animated, {
+  useAnimatedRef,
+  useSharedValue,
+  useAnimatedScrollHandler,
+} from 'react-native-reanimated';
+import data from './src/data/data';
+import RenderItem from './src/components/RenderItem';
 
 export default function App() {
+  const flatlistRef = useAnimatedRef();
+  const x = useSharedValue(0);
+
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      x.value = event.contentOffset.x;
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Hello Minar, Welcome to Onboarding Screen!</Text>
+      <Animated.FlatList
+        ref={flatlistRef}
+        onScroll={onScroll}
+        data={data}
+        renderItem={({ item, index }) => {
+          return <RenderItem item={item} index={index} x={x} />;
+        }}
+        keyExtractor={(item) => item.id}
+        scrollEventThrottle={16}
+        horizontal={true}
+        bounces={false}
+        pagingEnabled={true}
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   );
 }
@@ -11,8 +40,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/components/RenderItem.jsx
@@ -0,0 +1,98 @@
+import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
+import React from 'react';
+import LottieView from 'lottie-react-native';
+import Animated, {
+  useAnimatedStyle,
+  interpolate,
+  Extrapolate,
+} from 'react-native-reanimated';
+
+const RenderItem = ({ item, index, x }) => {
+  const { width: SCREEN_WIDTH } = useWindowDimensions();
+
+  const lottieAnimationStyle = useAnimatedStyle(() => {
+    const translateYAnimation = interpolate(
+      x.value,
+      [
+        (index - 1) * SCREEN_WIDTH,
+        index * SCREEN_WIDTH,
+        (index + 1) * SCREEN_WIDTH,
+      ],
+      [200, 0, -200],
+      Extrapolate.CLAMP
+    );
+
+    return {
+      transform: [{ translateY: translateYAnimation }],
+    };
+  });
+
+  const circleAnimation = useAnimatedStyle(() => {
+    const scale = interpolate(
+      x.value,
+      [
+        (index - 1) * SCREEN_WIDTH,
+        index * SCREEN_WIDTH,
+        (index + 1) * SCREEN_WIDTH,
+      ],
+      [1, 4, 4],
+      Extrapolate.CLAMP
+    );
+
+    return {
+      transform: [{ scale: scale }],
+    };
+  });
+
+  return (
+    <View style={[styles.itemContainer, { width: SCREEN_WIDTH }]}>
+      <View style={styles.circleContainer}>
+        <Animated.View
+          style={[
+            {
+              width: SCREEN_WIDTH,
+              height: SCREEN_WIDTH,
+              backgroundColor: item.backgroundColor,
+              borderRadius: SCREEN_WIDTH / 2,
+            },
+            circleAnimation,
+          ]}
+        />
+      </View>
+      <Animated.View style={lottieAnimationStyle}>
+        <LottieView
+          source={item.animation}
+          style={{ width: SCREEN_WIDTH * 0.9, height: SCREEN_WIDTH * 0.9 }}
+          autoPlay
+          loop
+        />
+      </Animated.View>
+      <Text style={[styles.itemText, { color: item.textColor }]}>
+        {item.text}
+      </Text>
+    </View>
+  );
+};
+
+export default RenderItem;
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginBottom: 120,
+  },
+  itemText: {
+    textAlign: 'center',
+    fontSize: 44,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    marginHorizontal: 20,
+  },
+  circleContainer: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+});
